9b78aaffa3005bd3b55c449480b29e696b97a382
Matt <craftymatt2gaming@gmail.com>
2019-11-23 17:22:49 +0000

    Update index.js

--- ThirdPartyDevelopers/index.js
+++ ThirdPartyDevelopers/index.js
@@ -1,11 +1,11 @@
-// Universal Discord Bot
+// Zeltux
 
 // Third Party Developers Copy
 
 // Created By Matt
 // In collaboration with Azono & amaan1028
 
-// Version 1.0.1
+// For Zeltux Version 1.0.6
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -29,60 +29,85 @@ fs.readdir("./events/", (err, files) => {
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
-fs.readdir(`./commands/modules/`, (err, files) => {
-  files.forEach((f, i) =>{
-  let props = require(`./commands/modules/${f}`)
-  console.log(`Loaded module ${f}.`);
-  try{
-  if (props.cmd8 && !props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  if (!props.cmd8 && props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  client.commands.set(props.cmd1.help.name, props.cmd1);}
-  catch{return}
-  try{
-  if (props.cmd8 && !props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  if (!props.cmd8 && props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  client.commands.set(props.cmd2.help.name, props.cmd2);}
-  catch{return}
-  try{
-  if (props.cmd8 && !props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  if (!props.cmd8 && props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  client.commands.set(props.cmd3.help.name, props.cmd3);}
-  catch{return}
-  try{
-  if (props.cmd8 && !props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  if (!props.cmd8 && props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  client.commands.set(props.cmd4.help.name, props.cmd4);}
-  catch{return}
-  try{
-  if (props.cmd8 && !props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  if (!props.cmd8 && props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  client.commands.set(props.cmd5.help.name, props.cmd5);}
-  catch{return}
-  try{
-  if (props.cmd8 && !props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  if (!props.cmd8 && props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  client.commands.set(props.cmd6.help.name, props.cmd6);}
-  catch{return}
-  try{
-  if (props.cmd8 && !props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  if (!props.cmd8 && props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  client.commands.set(props.cmd7.help.name, props.cmd7);}
-  catch{return}
-  try{
-  if (props.cmd8 && !props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  if (!props.cmd8 && props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  client.commands.set(props.cmd8.help.name, props.cmd8);}
-  catch{return}
-  try{
-    if (props.cmd8 && !props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-    if (!props.cmd8 && props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  client.commands.set(props.cmd9.help.name, props.cmd9);}
-  catch{return}
-  try{
-    if (props.cmd8 && !props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-    if (!props.cmd8 && props.cmd8.help.name) return console.log('Recieved an error loading commands in the module: ' + f)
-  client.commands.set(props.cmd10.help.name, props.cmd10);}
-  catch{return}
-  });});
+const modules = ['modules']
+
+modules.forEach(c => {
+  fs.readdir(`./commands/${c}/`, (err, files) => {
+  if (err) throw err;
+
+  if(c === 'modules'){
+
+    files.forEach((f, i) =>{
+      let props = require(`./commands/modules/${f}`)
+      if(f.split(".").pop() === "js"){
+      console.log(`Loaded ${f}.`);
+      try{
+      if (props.cmd1 && !props.cmd1.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      if (!props.cmd1 && props.cmd1.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      client.commands.set(props.cmd1.help.name, props.cmd1);
+      }
+      catch{return}
+      try{
+      if (props.cmd2 && !props.cmd2.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      if (!props.cmd2 && props.cmd2.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      client.commands.set(props.cmd2.help.name, props.cmd2);
+      }
+      catch{return}
+      try{
+      if (props.cmd3 && !props.cmd3.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      if (!props.cmd3 && props.cmd3.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      client.commands.set(props.cmd3.help.name, props.cmd3);
+      }
+      catch{return}
+      try{
+      if (props.cmd4 && !props.cmd4.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      if (!props.cmd4 && props.cmd4.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      client.commands.set(props.cmd4.help.name, props.cmd4);
+      }
+      catch{return}
+      try{
+      if (props.cmd5 && !props.cmd5.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      if (!props.cmd5 && props.cmd5.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      client.commands.set(props.cmd5.help.name, props.cmd5);
+      }
+      catch{return}
+      try{
+      if (props.cmd6 && !props.cmd6.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      if (!props.cmd6 && props.cmd6.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      client.commands.set(props.cmd6.help.name, props.cmd6);
+      }
+      catch{return}
+      try{
+      if (props.cmd7 && !props.cmd7.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      if (!props.cmd7 && props.cmd7.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      client.commands.set(props.cmd7.help.name, props.cmd7);
+      }
+      catch{return}
+      try{
+      if (props.cmd8 && !props.cmd8.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      if (!props.cmd8 && props.cmd8.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      client.commands.set(props.cmd8.help.name, props.cmd8);
+      }
+      catch{return}
+      try{
+      if (props.cmd9 && !props.cmd9.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      if (!props.cmd9 && props.cmd9.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      client.commands.set(props.cmd9.help.name, props.cmd9);
+      }
+      catch{return}
+      try{
+      if (props.cmd10 && !props.cmd10.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      if (!props.cmd10 && props.cmd10.help.name) return console.log('[ERROR] Recieved an error loading commands in the module: ' + f + ' Please contact Zeltux support.')
+      client.commands.set(props.cmd10.help.name, props.cmd10);
+      }
+      catch{return}
+     }});
+    console.log(`Succesfully Started!`)
+  }
+  else{
+  console.log(`[ERROR] A file has been put in the wrong location!`)
+  }
+  });
+});
 
 client.login(client.config.token);
