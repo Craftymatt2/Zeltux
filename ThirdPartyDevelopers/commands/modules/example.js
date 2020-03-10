1f8359651a90df6d5c5fb7ee50561f642e9e0dc8
Matt <craftymatt2gaming@gmail.com>
2020-03-10 20:05:37 +0000

    Update example.js

--- ThirdPartyDevelopers/commands/modules/example.js
+++ ThirdPartyDevelopers/commands/modules/example.js
@@ -1,99 +1,32 @@
-// Command 1
-exports.cmd1 = async (client, message, args) => {
-    message.channel.send("example1 works");
-  };
-  
-  exports.cmd1.help = {
-    name: "example1",
-    aliases: ['ex1']
-  };
-  
-  // Command 2
-  exports.cmd2 = async (client, message, args) => {
-    message.channel.send("example2 works");
-  };
-  
-  exports.cmd2.help = {
-    name: "example2",
-    aliases: ['ex2']
-  };
-  
-  // Command 3
-  exports.cmd3 = async (client, message, args) => {
-    message.channel.send("example3 works");
-  };
-  
-  exports.cmd3.help = {
-    name: "example3",
-    aliases: ['ex3']
-  };
-  
-  // Command 4
-  exports.cmd4 = async (client, message, args) => {
-  message.channel.send("example4 works");
-  };
-  
-  exports.cmd4.help = {
-    name: "example4",
-    aliases: ['ex4']
-  };
-  
-  // Command 5
-  exports.cmd5 = async (client, message, args) => {
-    message.channel.send("example5 works");
-  };
-    
-  exports.cmd5.help = {
-    name: "example5",
-    aliases: ['ex5']
-  };
-  
-  // Command 6
-  exports.cmd6 = async (client, message, args) => {
-    message.channel.send("example6 works");
-  };
-  
-  exports.cmd6.help = {
-    name: "example6",
-    aliases: ['ex6']
-  };
-  
-  // Command 7
-  exports.cmd7 = async (client, message, args) => {
-    message.channel.send("example7 works");
-  };
-  
-  exports.cmd7.help = {
-    name: "example7",
-    aliases: ['ex7']
-  };
-  
-  // Command 8
-  exports.cmd8 = async (client, message, args) => {
-    message.channel.send("example8 works");
-  };
-  
-  exports.cmd8.help = {
-    name: "example8",
-    aliases: ['ex8']
-  };
-  
-  // Command 9
-  exports.cmd9 = async (client, message, args) => {
-    message.channel.send("example9 works");
-  };
-  
-  exports.cmd9.help = {
-    name: "example9",
-    aliases: ['ex9']
-  };
-  
-  // Command 10
-  exports.cmd10 = async (client, message, args) => {
-    message.channel.send("example10 works");
-  };
-  
-  exports.cmd10.help = {
-    name: "example10",
-    aliases: ['ex10']
-  };
+const Discord = require("discord.js")
+
+
+// Define all commands
+commands = [
+    {
+        name: 'test',
+        run: async (client, message, args) => {
+            message.channel.send('Test')
+        }
+    },
+    {
+        name: 'test1',
+        run: async (client, message, args) => {
+            message.channel.send('Test1')
+        }
+    }
+]
+
+
+// Define all events
+events = [
+    {
+        name: 'message',
+        run: async (client, message, args) => {
+            message.channel.send('Message event ran')
+        }
+    }
+]
+
+
+module.exports = { commands, events }
