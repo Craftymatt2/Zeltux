5116c9ebb36d382708acd96c5366bd5407fe1eed
Matt <craftymatt2gaming@gmail.com>
2020-03-10 20:02:28 +0000

    Update message.js

--- ThirdPartyDevelopers/events/message.js
+++ ThirdPartyDevelopers/events/message.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = (client, message) => {
     if (message.author.bot) return;
+    if (!message.guild) return;
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     const cmd = client.commands.get(command)
